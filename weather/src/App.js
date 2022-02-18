12c23b26748349e1882bde28ccaeb3c8c5789761
volha <v.khviadchenia@gmail.com>
2022-02-18 04:22:30 +0100

    feat: add template for ButtonsPanel

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRotateRight, } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='wrapper d-flex justify-content-center'>
+      <header className='header d-flex justify-content-between'>
+        <div className='ButtonsPanel'>
+          <div class="btn-group mr-2" role="group" aria-label="First group">
+            <button type="button" class="btn bg-transparent btn-outline-light">
+            <FontAwesomeIcon icon={faArrowRotateRight} />
+            </button>
+          </div>
+
+          <div class="btn-group mr-4" role="group" aria-label="Second group">
+            <button type="button" class="btn bg-transparent btn-outline-light">EN</button>
+            <button type="button" class="btn bg-transparent btn-outline-light">RU</button>
+            <button type="button" class="btn bg-transparent btn-outline-light">BY</button>
+          </div>
+
+          <div class="btn-group mr-2" role="group" aria-label="Third group">
+            <button type="button" class="btn bg-transparent btn-outline-light">°F</button>
+            <button type="button" class="btn bg-transparent btn-outline-light">°C</button>
+          </div>
+        </div>
+        <div className='SearchPanel'>2</div>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
