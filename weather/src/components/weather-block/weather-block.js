72246c9fc4bc344aa837d6de59aec55c29cc0379
volha <v.khviadchenia@gmail.com>
2022-02-22 04:54:26 +0100

    feat: update component weather-block

--- weather/src/components/weather-block/weather-block.js
+++ weather/src/components/weather-block/weather-block.js
@@ -2,13 +2,34 @@ import React from "react";
 import './weather-block.css';
 
 function WeatherBlock() {
+  const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  let today = new Date();
+  let getDate = () => {
+    let hours = today.getHours().toString().length < 2 ? `0${today.getHours()}`: today.getHours();
+    let minutes = today.getMinutes().toString().length < 2 ? `0${today.getMinutes()}`: today.getMinutes();
+    let seconds = today.getSeconds().toString().length < 2 ? `0${today.getSeconds()}`: today.getSeconds();
+    return `${week[today.getDay()]} ${today.getDate()} ${months[today.getMonth()]} ${hours}:${minutes}:${seconds}`;
+  };
+  let currentDay = getDate();
+
   return (
     <div className='weather-block'>
     <p>Minsk, Belarus</p>
-    <p>
-      <span>Mon 28 October</span>
-      <span>12.13</span>
-    </p>
+    <p className="date">{currentDay}</p>
     <div className='row'>
       <div className='col'>10°</div>
       <div className='col'>
