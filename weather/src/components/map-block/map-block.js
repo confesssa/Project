442339b69a2b526e8a94692683010ec91ddbe85f
volha <v.khviadchenia@gmail.com>
2022-03-24 23:59:32 +0100

    feat: add MapBlock

--- weather/src/components/map-block/map-block.js
+++ weather/src/components/map-block/map-block.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import './map-block.css';
 
 function MapBlock() {
+  let crd;
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    crd = pos.coords;
+
+    // console.log(crd);
+
+    // console.log('Ваше текущее местоположение:');
+    // console.log(`Широта: ${crd.latitude}`);
+    // console.log(`Долгота: ${crd.longitude}`);
+    // console.log(`Плюс-минус ${crd.accuracy} метров.`);
+
+    return crd;
+    // const latitude = crd.latitude;
+    // const longitude = crd.longitude;
+  };
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  };
+  
+  const [position, setState] = useState(navigator.geolocation.getCurrentPosition(success, error, options));
+  console.log(position);
+
   return (
     <div className='map-block'>
       <div className='image'></div>
-      <p>Latitude: <span>53.32</span></p>
-      <p>Longitude: <span>32.34</span></p>
+      <p>Latitude: <span>{position.latitude}</span></p>
+      <p>Longitude: <span>{position.longitude}</span></p>
   </div>
   )
 }
